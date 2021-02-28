edbbeade9a5226bdc787726a5b2bb1f07336471d
reimibeta <pinsoneatra.soneatra846@gmail.com>
2021-02-28 13:47:51 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# django-rest-framework-utils
+ django rest framework utils
---
+++ rest_framework_utils/renders.py
@@ -0,0 +1,5 @@
+from rest_framework.renderers import JSONRenderer
+
+
+class UTF8JSONRender(JSONRenderer):
+    charset = 'utf-8'
