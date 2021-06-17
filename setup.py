503852d3fda3717b0de0932a216d27caa668e743
reimibeta <pinsoneatra.soneatra846@gmail.com>
2021-06-17 15:22:36 +0700

    .

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ import os
 import setuptools
 
 setuptools.setup(
-    name='rest_framework_utils',
-    version='1.0.1',
+    name='django_rest_framework',
+    version='1.0.2',
     packages=setuptools.find_packages()
 )
